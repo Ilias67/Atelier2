5fe12c653db15f72d2bd8043cd42a3c43b4baaf4
unknown <ils.mamouny@gmail.com>
2021-06-17 01:09:45 +0100

    commit

---
+++ CompteCourant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atelier2
+{
+    class CompteCourant : Compte
+    {
+        private MAD Decouvert;
+        public CompteCourant(Client client,MAD solde, MAD decouvert) :base(client,solde)
+        {
+            this.Decouvert = decouvert;
+        }
+
+        public override bool Debiter(MAD Somme)//la redéfinition de la méthode débiter
+        {
+            if (this.ComparerDecouvert(Somme,Decouvert))
+            {
+                return base.Debiter(Somme);
+            }return false;
+
+            
+        }
+    }
+}
